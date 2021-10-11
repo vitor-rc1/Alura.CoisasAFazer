f5131dc3ea489228b6076f03220d498bc8fc62d8
Vitor R <vitor_rc@outlook.com>
2021-10-11 17:13:07 -0300

    Teste - falha ao cadastrar tarefa

---
+++ src/Alura.CoisasAFazer.Services/Handlers/CommandResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Alura.CoisasAFazer.Services.Handlers
+{
+    public class CommandResult
+    {
+        public CommandResult(bool success)
+        {
+            IsSuccess = success;
+        }
+
+        public bool IsSuccess { get; }
+    }
+}
--- tests/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
+++ tests/Alura.CoisasAFazer.Testes/CadastraTarefaHandlerExecute.cs
@@ -3,6 +3,7 @@ using Alura.CoisasAFazer.Core.Models;
 using Alura.CoisasAFazer.Infrastructure;
 using Alura.CoisasAFazer.Services.Handlers;
 using Microsoft.EntityFrameworkCore;
+using Moq;
 using System;
 using System.Linq;
 using Xunit;
@@ -21,7 +22,7 @@ namespace Alura.CoisasAFazer.Testes
                 new DateTime(2019, 12, 31));
 
             var options = new DbContextOptionsBuilder<DbTarefasContext>()
-                .UseInMemoryDatabase("DbTarefasContext")
+                .UseInMemoryDatabase("DbTarefasContext2")
                 .Options;
             var context = new DbTarefasContext(options);
             var repo = new RepositorioTarefa(context);
@@ -34,6 +35,31 @@ namespace Alura.CoisasAFazer.Testes
             // Assert
             var tarefa = repo.ObtemTarefas(t => t.Titulo == "Estudar XUnit").FirstOrDefault();
             Assert.NotNull(tarefa);
+        }
+
+        [Fact]
+        public void QuandoExceptionForLancadaResultadoIsSuccessDeveSerFalse()
+        {
+            // Arrange
+            var comando = new CadastraTarefa(
+                "Estudar XUnit",
+                new Categoria("Estudo"),
+                new DateTime(2019, 12, 31));
+
+            var mock = new Mock<IRepositorioTarefas>();
+
+            mock.Setup(r => r.IncluirTarefas(It.IsAny<Tarefa[]>()))
+                .Throws(new Exception("Houve um erro na inclusão de tarefas"));
+
+            var repo = mock.Object;
+
+            var handler = new CadastraTarefaHandler(repo);
+
+            // Act
+            CommandResult resultado = handler.Execute(comando);
+
+            // Assert
+            Assert.False(resultado.IsSuccess);
         }
     }
 }
